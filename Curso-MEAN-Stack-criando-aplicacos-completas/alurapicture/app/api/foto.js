ace252b844398a386cdd68863b16256b2651df0a
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2016-08-16 15:54:11 -0300

    Aula 10 - Qual é o esquema?

--- Curso-MEAN-Stack-criando-aplicacos-completas/alurapicture/app/api/foto.js
+++ Curso-MEAN-Stack-criando-aplicacos-completas/alurapicture/app/api/foto.js
@@ -1,49 +1,47 @@
+var mongoose = require('mongoose');
 var api = {};
 
-var CONTADOR = 2;
-
-var fotos = [
-   {_id: 1, titulo: 'Leão', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'},
-   {_id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'}
-];
-
 api.lista = function(req, res){
-   res.json(fotos);
+
+   var model = mongoose.model('Foto');
+
+   model.find({})
+         .then(function(fotos){
+            res.json(fotos);
+         }, function(error){
+            console.log(error);
+            res.status(500).json(error);
+         });
 };
 
 api.buscaPorId = function(req, res){
-   var foto = fotos.find(function(foto){
-      return foto._id == req.params.id;
-   })
-   res.json(foto);
+   var model = mongoose.model('Foto');
+   model.findOne({_id: req.params.id})
+      .then(function(foto){
+         res.json(foto);
+      }, function(error){
+         console.log(error);
+         res.status(500).json(error);
+      });
 };
 
 api.removePorId = function(req, res){
-   fotos = fotos.filter(function(foto){
-      return foto._id != req.params.id;
-   });
-   res.sendStatus(204);
+   var model = mongoose.model('Foto');
+   model.remove({_id: req.params.id})
+      .then(function(foto){
+         res.sendStatus(204);
+      }, function(error){
+         console.log(error);
+         res.status(500).json(error);
+      });
 };
 
 api.adiciona = function(req, res){
-   var foto = req.body;
-   foto._id = ++CONTADOR;
-   fotos.push(foto);
 
-   res.json(foto);
 };
 
 api.atualiza = function(req, res){
-   var foto = req.body;
-   var fotoId = req.params.id;
 
-   var indece = fotos.findIndex(function(foto){
-      return foto._id == fotoId;
-   });
-
-   fotos[indece] = foto;
-
-   res.sendStatus(200);
 };
 
 module.exports = api;
