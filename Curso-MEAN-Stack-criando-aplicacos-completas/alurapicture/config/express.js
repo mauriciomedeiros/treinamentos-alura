dea3c81c1e713cd51a53c9889e98101b3cbd2e7d
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2016-08-17 15:57:33 -0300

    Aula 11 - Autenticação baseada em token

--- Curso-MEAN-Stack-criando-aplicacos-completas/alurapicture/config/express.js
+++ Curso-MEAN-Stack-criando-aplicacos-completas/alurapicture/config/express.js
@@ -3,14 +3,15 @@ var consign = require('consign');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.set('secret', 'chavetoken');
 
 app.use(express.static('./public'));
-
 app.use(bodyParser.json());
 
 consign({cwd: 'app'})
    .include('models')
    .then('api')
+   .then('routes/auth.js')
    .then('routes')
    .into(app);
 
